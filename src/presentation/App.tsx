2b832125075b43b0c03070107e979cbff4d65aba
zavvdev <zaviryukha.igor99@gmail.com>
2024-05-24 14:00:34 +0300

    added readme

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
 import "~/presentation/styles/css/index.css";
+import { ErrorBoundary } from "~/presentation/ErrorBoundary";
 
 export function App() {
   const name: string = "world";
 
   return (
-    <>
-      <h1>hello, {name}</h1>
-      <p>env: {import.meta.env.VITE_SERVER_API_ENDPOINT}</p>
-    </>
+    <ErrorBoundary>
+      <Suspense fallback={<div />}>
+        <h1>hello, {name}</h1>
+        <p>env: {import.meta.env.VITE_SERVER_API_ENDPOINT}</p>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
